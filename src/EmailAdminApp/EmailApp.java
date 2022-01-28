0586d4ce38bdc1a965caf7f36e5ff1787e60d883
coderylicode <rylidavis94@gmail.com>
2022-01-27 23:01:58 -0800

    Java Complete Project Finished

---
+++ src/EmailAdminApp/EmailApp.java
@@ -0,0 +1,14 @@
+package EmailAdminApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+				
+		// Creating a new employee (i.e. an object of the Email class)
+		Email em1 = new Email("John", "Doe");
+		System.out.println(em1.showInfo());
+	}
+
+}
+		
+	
